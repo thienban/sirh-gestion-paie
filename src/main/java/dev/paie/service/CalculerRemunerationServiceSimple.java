07e598f544d861cf045eed92479688f092afbb05
thien ban BUI <thienban91@hotmail.fr>
2017-12-18 15:57:23 +0100

    debut tp 2

---
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -0,0 +1,19 @@
+package dev.paie.service;
+
+import dev.paie.entite.BulletinSalaire;
+import dev.paie.entite.ResultatCalculRemuneration;
+
+public class CalculerRemunerationServiceSimple implements CalculerRemunerationService{
+	
+	public CalculerRemunerationServiceSimple () {
+		
+	}
+	@Override
+	public ResultatCalculRemuneration calculer(BulletinSalaire bulletin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
