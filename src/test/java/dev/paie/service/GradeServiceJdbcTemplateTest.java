ccc7a540c817933b9e2958cf6a92676444eb2e8c
thien ban BUI <thienban91@hotmail.fr>
2017-12-19 12:23:51 +0100

    realise les test

--- src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
+++ src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
@@ -3,6 +3,8 @@ package dev.paie.service;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import dev.paie.entite.Grade;
+
 //TODO compléter la configuration
 public class GradeServiceJdbcTemplateTest {
 	@Autowired
@@ -10,7 +12,22 @@ public class GradeServiceJdbcTemplateTest {
 
 	@Test
 	public void test_sauvegarder_lister_mettre_a_jour() {
+		// TODO sauvegarder un nouveau grade
+		grade1 = new Grade(1,"",new BigDecial , new BigDecimal);
+		gradeService.sauvegarder(grade1));
+		// TODO vérifier qu'il est possible de récupérer le nouveau grade 
+		grade2 = new 
+		lister();		
+		// TODO modifier un grade
+		grade3 = new
+		mettreAJour();
+		// TODO vérifier que les modifications sont bien prises en compte
+		grade4 = new 
+		lister();
+	
 		
+	
+	
 		   
 	}
 }
